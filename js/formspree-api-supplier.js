ba257ebbc6e34f2232549ed478bab087b97fa102
AshwinShetty3 <as9932418@gmail.com>
2025-07-06 15:30:48 +0530

    setUp Become Suplier & contact US  emails sending in all the pages

--- js/formspree-api-supplier.js
+++ js/formspree-api-supplier.js
@@ -1,58 +1,51 @@
 window.addEventListener("DOMContentLoaded", function() {
-
-  var timeNow = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})
-   const CC_EMAIL = "bhakti.kadam@wgblindia.com"
-  //const CC_EMAIL = "rohan.kavathekar@wgblindia.com"
-  const SUBJECT = "Pisum Exports: Become Supplier Enquiry On " + timeNow
-  var form = document.getElementById("supplier-form-modal")
-  var button = document.getElementById("supplier-submit")
-  var status = document.getElementById("supplier-toast")
-  form.action = "https://formspree.io/mnqgzwdy"
-
-  form._cc.value = CC_EMAIL
-  form._subject.value = SUBJECT
+  var form = document.getElementById("supplier-form-modal");
+  var status = document.getElementById("supplier-toast");
+  form.action = "https://formcarry.com/s/Dc1rp6BHI-d";
 
   function success() {
     form.reset();
-    status.innerHTML = "Thank you. We have received your message. Our Expert will revert you back shortly."
-    status.style.color = 'green'
-    submit.disabled = true
+    status.innerHTML = "Message has been sent successfully!";
+    status.style.color = 'green';
+    status.style.display = 'block';
   }
 
   function error() {
-    status.innerHTML = 'We apologies for your inconvenience that enquiry has not been received to us please contact us to contact@pisumfood.com / 7219115858.'
-    status.style.color = 'red'
-    submit.disabled = false
+    status.innerHTML = 'There was an error sending your message. Please try again.';
+    status.style.color = 'red';
+    status.style.display = 'block';
   }
 
   form.addEventListener("submit", function(ev) {
-    ev.preventDefault()
-    status.innerHTML = 'sending..'
-    status.style.color = 'orange'
+    ev.preventDefault();
+    status.innerHTML = 'Sending...';
+    status.style.color = 'orange';
+    status.style.display = 'block';
 
     var isFormValid = $("#supplier-form-modal").valid();
     if (isFormValid) {
       var data = new FormData(form);
-      ajax( form.method, form.action, data, success, error)
-    }
-    else {
-      status.innerHTML = 'Please Make Sure That you have Entered correct Data'
-      status.style.color = 'red'
+      fetch(form.action, {
+        method: "POST",
+        body: data,
+        headers: {
+          'Accept': 'application/json'
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          success();
+        } else {
+          error();
+        }
+      })
+      .catch(() => {
+        error();
+      });
+    } else {
+      status.innerHTML = 'Please Make Sure That you have Entered correct Data';
+      status.style.color = 'red';
+      status.style.display = 'block';
     }
   });
 });
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json")
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType)
-    }
-  }
-  xhr.send(data)
-}
